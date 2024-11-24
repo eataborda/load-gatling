bd977cb058f57a57b882001832f4fb9c98c01c49
Edwin Taborda <eataborda.m@gmail.com>
2024-11-24 15:53:42 -0500

    General repository update

--- src/gatling/scala/com/github/eataborda/gatling/basic/BasicSimulation.scala
+++ src/gatling/scala/com/github/eataborda/gatling/basic/BasicSimulation.scala
@@ -28,7 +28,7 @@ class BasicSimulation extends Simulation {
 
   setUp(
     getDellComputersScenario.inject(
-      nothingFor(5.seconds),
+      nothingFor(6.seconds),
       atOnceUsers(5),
       rampUsers(20).during(10.seconds)
     ).protocols(httpProtocol))
